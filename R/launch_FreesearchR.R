1b31743898b177d2283bdfd66d3347ee468856d0
Andreas Gammelgaard Damsbo <andreas@gdamsbo.dk>
2026-02-23 13:21:07 +0100

    feat: launch function prepared for optional limits to data import. this will also provide future options for customisations

--- R/launch_FreesearchR.R
+++ R/launch_FreesearchR.R
@@ -3,6 +3,11 @@
 #' @description
 #' All data.frames in the global environment will be accessible through the app.
 #'
+#' @param include_globalenv flag to include global env (local data) as option
+#' when loading data
+#' @param data_limit_default default data set observations limit
+#' @param data_limit_upper data set observations upper limit
+#' @param data_limit_lower data set observations lower limit
 #' @param ... passed on to `shiny::runApp()`
 #'
 #' @returns shiny app
@@ -13,13 +18,24 @@
 #' data(mtcars)
 #' launch_FreesearchR(launch.browser = TRUE)
 #' }
-launch_FreesearchR <- function(...){
+launch_FreesearchR <- function(inlcude_globalenv = TRUE,
+                               data_limit_default = 1000,
+                               data_limit_upper = 100000,
+                               data_limit_lower = 1,
+                               ...) {
+  global_freesearchR <- list(
+    include_globalenv = include_globalenv,
+    data_limit_default = data_limit_default,
+    data_limit_upper = data_limit_upper,
+    data_limit_lower = data_limit_lower
+  )
+
   appDir <- system.file("apps", "FreesearchR", package = "FreesearchR")
   if (appDir == "") {
-    stop("Could not find the app directory. Try re-installing `FreesearchR`.", call. = FALSE)
+    stop("Could not find the app directory. Try re-installing `FreesearchR`.",
+         call. = FALSE)
   }
 
-  a <- shiny::runApp(appDir = paste0(appDir,"/app.R"), ...)
+  a <- shiny::runApp(appDir = paste0(appDir, "/app.R"), ...)
   return(invisible(a))
 }
-
